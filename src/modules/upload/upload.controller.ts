1ec60bcdb5ce1d6e5b204818ba73a6bcee08f9cd
jayemscript <johnmarkpulmano052802@gmail.com>
2026-07-18 18:59:48 +0800

    feat: added mvp working demo for aws s3 upload module

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -6,12 +6,14 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    await this.uploadService.upload(file.originalname, file.buffer);
   }
 }
